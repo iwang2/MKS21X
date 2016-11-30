d01089240bc4b98c5c172ff28853f59c990f7e70
Ida <iwang2@stuy.edu>
2016-11-30 06:24:45 -0500

    variables, constructor, getters/setters

--- 01Library/CirculatingBook.java
+++ 01Library/CirculatingBook.java
@@ -1,2 +1,24 @@
 public class CirculatingBook extends LibraryBook{
+    private String currentHolder;
+    private string dueDate;
+
+    public CirculatingBook(String name, String book, String number, String call){
+	super(name,book,number,call);
+	currentHolder = "";
+	dueDate = "";
+    }
+
+    public String getCurrentHolder(){
+	return currentHolder;
+    }
+    public void setCurrentHolder(String name){
+	currentHolder = name;
+    }
+    public String getDueDate(){
+	return dueDate;
+    }
+    public void setDueDate(String date){
+	dueDate = date;
+    }
+
 }
