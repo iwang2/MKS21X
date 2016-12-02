ef86dacb69e1492aa1583276767fcd4ca312f396
Ida <iwang2@stuy.edu>
2016-12-02 06:31:16 -0500

    empty constructors and methods

---
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -0,0 +1,17 @@
+import java.util.Iterator;
+
+public class SuperArrayIterator implements Iterator<String>{
+    SuperArray a;
+    int index;
+
+    public SuperArrayIterator(){
+
+    }
+
+    public boolean hasNext(){
+    }
+    public void remove(){
+	throw new UnsupportedOperationException();
+    }
+ 
+}
