c6ea387d07618233ec5588b0fcd911e77a6b30a5
Ida Wang <iwang2@stuy.edu>
2016-12-07 22:40:34 -0500

    empty compareTo()

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,4 +1,4 @@
-public class Barcode implement Comparable<Barcode>{
+public class Barcode implements Comparable<Barcode>{
     private String zip;
     private int checkDigit;
 
@@ -9,15 +9,62 @@ public class Barcode implement Comparable<Barcode>{
        - zip and checkDigit are initialized
     */
     public Barcode(String zip){
-	this.zip = zip;
-	checkDigit = checkDigit();
+	if(zip.length() == 5){
+	    this.zip = zip;
+	}else{
+	    throw new IllegalArgumentException();
+	}
+        try{
+	    checkDigit = checkDigit();
+	}catch(NumberFormatException e){
+	    throw new IllegalArgumentException();
+	}
     }
 
-    public int checkDigit(){
+    private int checkDigit(){
 	int sum = 0;
 	for(int i = 0; i < 5; i++){
 	    sum += Integer.parseInt(""+zip.charAt(i));
 	}
 	return sum%10;
     }
+
+    public Barcode clone(){
+	Barcode a = new Barcode(this.zip);
+	return a;
+    }
+
+    public String toString(){
+	String barcode = "";
+	String numbers = zip + checkDigit;
+	for(int i = 0; i < 6; i++){
+	    if(numbers.charAt(i)=='1'){
+		barcode += :::|| ;
+	    }else if(numbers.charAt(i)=='2'){
+		barcode += ::|:| ;
+	    }else if(numbers.charAt(i)=='3'){
+		barcode += ::||: ;
+	    }else if(numbers.charAt(i)=='4'){
+		barcode += :|::| ;
+	    }else if(numbers.charAt(i)=='5'){
+		barcode += :|:|: ;
+	    }else if(numbers.charAt(i)=='6'){
+		barcode += :||:: ;
+	    }else if(numbers.charAt(i)=='7'){
+		barcode += |:::| ;
+	    }else if(numbers.charAt(i)=='8'){
+		barcode += |::|: ;
+	    }else if(numbers.charAt(i)=='9'){
+		barcode += |:|:: ;
+	    }else if(numbers.charAt(i)=='0'){
+		barcode += ||::: ;
+	    }
+	}
+	return numbers + "  " + barcode;
+    }
+
+    public int compareTo(Barcode other){
+    }
+}
+    
 	
