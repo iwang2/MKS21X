8d68ce9abacf1dcb50a517bcf59305a440f80fd0
Ida <iwang2@stuy.edu>
2016-12-09 06:34:32 -0500

    array for code, empty toString()

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,8 +1,19 @@
 public class Barcode implements Comparable<Barcode>{
-    private String zip;
-    private int checkDigit;
-    private String total;
+    private String _zip;
+    private int _checkDigit;
+    private String total = _zip + _checkDigit;
 
+    private String[] codelist = {"||:::",  //0
+				 ":::||",  //1
+				 "::|:|",  //2
+				 "::||:",  //3
+				 ":|::|",  //4
+				 ":|:|:",  //5
+				 ":||::",  //6
+				 "|:::|",  //7
+				 "|::|:",  //8
+				 "|:|::"}; //9
+    
     /* precondition: zip.length() = 5 and zip contains only digits
        postcondition: throws a runtime excpetion
        - zip is not the correct length
@@ -11,62 +22,31 @@ public class Barcode implements Comparable<Barcode>{
     */
     public Barcode(String zip){
 	if(zip.length() == 5){
-	    this.zip = zip;
+	    _zip = zip;
 	}else{
-	    throw new IllegalArgumentException();
+	    throw new IllegalArgumentException("invalid length");
 	}
         try{
-	    checkDigit = checkDigit();
+	    _checkDigit = checkDigit();
 	}catch(NumberFormatException e){
-	    throw new IllegalArgumentException();
+	    throw new IllegalArgumentException("zip must be a String of numbers only");
 	}
-	total = zip + checkDigit;
+	total = _zip + _checkDigit;
     }
 
     private int checkDigit(){
 	int sum = 0;
 	for(int i = 0; i < 5; i++){
-	    sum += Integer.parseInt(""+zip.charAt(i));
+	    sum += Integer.parseInt(""+_zip.charAt(i));
 	}
 	return sum%10;
     }
 
-    public Barcode clone(){
-	Barcode a = new Barcode(this.zip);
-	return a;
-    }
-
     public String toString(){
-	String barcode = "";
-	String numbers = zip + checkDigit;
-	for(int i = 0; i < 6; i++){
-	    if(numbers.charAt(i)=='1'){
-		barcode += ":::||" ;
-	    }else if(numbers.charAt(i)=='2'){
-		barcode += "::|:|" ;
-	    }else if(numbers.charAt(i)=='3'){
-		barcode += "::||:" ;
-	    }else if(numbers.charAt(i)=='4'){
-		barcode += ":|::|" ;
-	    }else if(numbers.charAt(i)=='5'){
-		barcode += ":|:|:" ;
-	    }else if(numbers.charAt(i)=='6'){
-		barcode += ":||::" ;
-	    }else if(numbers.charAt(i)=='7'){
-		barcode += "|:::|" ;
-	    }else if(numbers.charAt(i)=='8'){
-		barcode += "|::|:" ;
-	    }else if(numbers.charAt(i)=='9'){
-		barcode += "|:|::" ;
-	    }else if(numbers.charAt(i)=='0'){
-		barcode += "||:::" ;
-	    }
-	}
-	return numbers + "  |" + barcode + "|";
     }
 
     public int compareTo(Barcode other){
-	return (this.total).compareTo(other.total);
+	return (this._zip).compareTo(other._zip);
     }
 }
     
