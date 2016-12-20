7dce9bcd9793c5b2b58a57292602231c89f9a3bb
Ida <iwang2@stuy.edu>
2016-12-20 06:34:27 -0500

    untested

---
+++ Temp/Temperature.java
@@ -0,0 +1,14 @@
+public class Temperature{
+    
+    public static double CtoF(double t){
+	return t * 1.8 + 32.0; 
+    }
+    
+    public static double FtoC(double t){
+	return (t - 32.0) / 1.8;
+    }
+
+    public static void main(String[]args){
+    }
+    
+}
