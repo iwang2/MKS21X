95ad48947bd7e75de369e286b8b39bc566f0452d
Ida <iwang2@stuy.edu>
2016-12-21 06:38:25 -0500

    finish

---
+++ Temp/Window.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+public class Window extends JFrame implements ActionListener{
+    private Container pane;
+
+    private JButton b;
+    private JLable l;
+    private JTextField t;
+
+    public Window(){
+}
