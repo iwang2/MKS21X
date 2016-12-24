34bccce701c156a0fd97ab86839fa8cdef943855
Ida Wang <iwang2@stuy.edu>
2016-12-23 22:54:51 -0500

    changed system look and feel

--- Temp/Window.java
+++ Temp/Window.java
@@ -47,6 +47,24 @@ public class Window extends JFrame implements ActionListener{
     }
     
     public static void main(String[]args){
+	
+	try {
+	// Set System L&F
+	    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+	} 
+	catch (UnsupportedLookAndFeelException e) {
+	    // handle exception
+	}
+	catch (ClassNotFoundException e) {
+	    // handle exception
+	}
+	catch (InstantiationException e) {
+	    // handle exception
+	}
+	catch (IllegalAccessException e) {
+	    // handle exception
+	}
+
 	Window a = new Window();
 	a.setVisible(true);
     }
